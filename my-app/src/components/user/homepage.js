ac58f631750a0a491fe3cc5a40edeab3aa470154
Thanh Phu <thanhphu8436@gmail.com>
2024-03-14 03:08:40 +0700

    again

--- my-app/src/components/user/homepage.js
+++ my-app/src/components/user/homepage.js
@@ -9,51 +9,33 @@ export default function HomePage() {
                 <PanelHome />
                 <div>
                     <div className="container">
-                        <div className="row">
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="support-wrap mb-30">
-                                    <div className="support-icon">
-                                        <img className="animated" src="./img/home/support-1.png" alt="" />
-                                    </div>
-                                    <div className="support-content">
-                                        <h5>Free Shipping</h5>
-                                        <p>Free shipping on all orders</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="support-wrap mb-30">
-                                    <div className="support-icon">
-                                        <img className="animated" src="./img/home/support-2.png" alt="" />
-                                    </div>
-                                    <div className="support-content">
-                                        <h5>Support 24/7</h5>
-                                        <p>24/7 customer support</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="support-wrap mb-30">
-                                    <div className="support-icon">
-                                        <img className="animated" src="./img/home/support-3.png" alt="" />
-                                    </div>
-                                    <div className="support-content">
-                                        <h5>Money Return</h5>
-                                        <p>Easy return policy</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6">
-                                <div className="support-wrap mb-30">
-                                    <div className="support-icon">
-                                        <img className="animated" src="./img/home/support-4.png" alt="" />
-                                    </div>
-                                    <div className="support-content">
-                                        <h5>Order Discount</h5>
-                                        <p>Discounts on orders</p>
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="support-icon">
+                            <i class="fa-solid fa-truck-fast"></i>
+                        </div>
+                        <div className="support-content">
+                            <h5>Free Shipping</h5>
+                            <p>Free shipping on all orders</p>
+                        </div>
+                        <div className="support-icon">
+                            <i class="fa-solid fa-headset"></i>
+                        </div>
+                        <div className="support-content">
+                            <h5>Support 24/7</h5>
+                            <p>24/7 customer support</p>
+                        </div>
+                        <div className="support-icon">
+                            <i class="fa-solid fa-money-bill-transfer"></i>
+                        </div>
+                        <div className="support-content">
+                            <h5>Money Return</h5>
+                            <p>Easy return policy</p>
+                        </div>
+                        <div className="support-icon">
+                            <i class="fa-brands fa-shopify"></i>
+                        </div>
+                        <div className="support-content">
+                            <h5>Order Discount</h5>
+                            <p>Discounts on orders</p>
                         </div>
                     </div>
                 </div>
@@ -61,7 +43,7 @@ export default function HomePage() {
                     <h2>FAMOUS BRANDS</h2>
                 </div>
                 <BrandsCarousel />
-                <div class="section-title text-center" style={{ padding: '50px', marginTop: '30px' }}>
+                <div class="section-title text-center" style={{ padding: '50px', marginTop: '0px' }}>
                     <h2>SẢN PHẨM NỔI BẬT</h2>
                 </div>
             </div>
